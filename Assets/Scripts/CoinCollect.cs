5b5d123cb636d210b89a79531f3a9ddfc6700eb2
Kudoshi <brendenguan@live.com>
2025-07-13 06:07:53 +0800

    fix: UI Coin

--- Assets/Scripts/CoinCollect.cs
+++ Assets/Scripts/CoinCollect.cs
@@ -6,17 +6,12 @@ using UnityEditor.UI;
 
 public class CoinCollect : MonoBehaviour
 {
-    public TextMeshProUGUI CoinAmount;
-
-    private int coin = 0;
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            coin++;
-            CoinAmount.text = "X" + coin.ToString();
+            UICoin.Instance.AddCoin();
             Destroy(gameObject);
         }
     }
